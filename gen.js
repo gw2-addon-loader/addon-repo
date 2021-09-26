ace9424c222f5ced5596f691ca866e2bd197683e
FriendlyFire <jpguertin.cg@gmail.com>
2021-09-26 00:32:50 -0400

    Added fetching of individual addons' version/download info hourly/on push.

--- gen.js
+++ gen.js
@@ -1,17 +1,49 @@
 import YAML from 'yaml';
 import * as fsp from 'fs/promises';
 import * as path from 'path';
+import axios from 'axios';
 
 console.log("Getting files and directories...");
 
 const dirname = path.dirname(process.argv[1]);
+const username = process.argv[2];
+const token = process.argv[3];
 
 let addons = {};
 
-function add_addon(name, file) {
+async function insert_github(addon) {
+    const { data } = await axios.get(addon.host_url, { auth: { username: username, password: token } });
+    addon.version_id = data.tag_name;
+    addon.download_url = data.assets[0].browser_download_url;
+}
+
+async function insert_standalone(addon) {
+    if (addon.version_url != null) {
+        const { data } = await axios.get(addon.version_url, { responseType: 'text', transitional: { forcedJSONParsing: false } });
+        addon.version_id = data;
+    }
+    addon.download_url = addon.host_url;
+}
+
+async function add_addon(name, file) {
     console.log("Loading " + name + " from YAML...");
-    const yaml = YAML.parse(file);
-    addons[name] = yaml;
+
+    try {
+        let yaml = YAML.parse(file);
+
+        try {
+            if (yaml.host_type === 'github')
+                await insert_github(yaml);
+            else if (yaml.host_type === 'standalone')
+                await insert_standalone(yaml);
+        } catch (ex) {
+            console.log("Error: Could not successfully fetch version information for addon '" + name + "': " + ex);
+        }
+        addons[name] = yaml;
+
+    } catch (ex) {
+        console.log("Error: Could not successfully parse YAML for addon '" + name + "': " + ex);
+    }
 }
 
 const files = await fsp.readdir(dirname, { withFileTypes: true });
@@ -21,7 +53,7 @@ for (const file of files) {
 
     try {
         const update_file = await fsp.readFile(dirname + "/" + file.name + "/update.yaml", "utf-8");
-        add_addon(file.name, update_file);
+         await add_addon(file.name, update_file);
         continue;
     } catch (ex) {
         if(ex.code !== 'ENOENT')
@@ -32,7 +64,7 @@ for (const file of files) {
 
     try {
         const update_file = await fsp.readFile(dirname + "/" + file.name + "/update-placeholder.yaml", "utf-8");
-        add_addon(file.name, update_file);
+        await add_addon(file.name, update_file);
         continue;
     } catch (ex) {
         if(ex.code !== 'ENOENT')
